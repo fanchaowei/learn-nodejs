3e50b8fa5ce5f9c7c13631ecfb23b9494e63d88a
fanchaowei <597064177@qq.com>
2023-06-25 21:03:44 +0800

    feat: process 实现带参命令

--- bullshit_generator/index.js
+++ bullshit_generator/index.js
@@ -1,49 +1,58 @@
-import { readFileSync, writeFileSync, existsSync, mkdirSync } from 'fs'
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
 import generator from './lib/generator.js'
 import { createRandomPicker } from './lib/random.js'
-import moment from 'moment'
+import usage from './helper/index.js'
+import { loadCorpus, saveCorpus } from './lib/corpus.js'
+
+import commandLineArgs from 'command-line-args'
 ;(function () {
-  // 获得当前文件的绝对路径
-  const url = import.meta.url
-  // fileURLToPath 将 file:// 协议的 URL 转换成绝对路径(ES Modules 规范)。
-  // dirname 获得文件所在的目录，如果是 CommonJS 规范，可以直接使用 __dirname 获取当前文件所在的目录的路径
-  // 这里自己实现一个 __dirname
-  const __dirname = dirname(fileURLToPath(url))
-  // 获取语料库
-  const corpus = loadCorpus(`corpus/data.json`)
-  // 获得生成文章标题函数
-  const pickTitle = createRandomPicker(corpus.title)
-  // 生成文章标题
-  const title = pickTitle()
-  // 生成文章
-  const article = generator(title, { corpus })
-  // 保存文章
-  saveCorpus(title, article)
+  main()
 
-  // 获取语料库
-  function loadCorpus(src) {
-    // resolve 将目录和文件名拼接成绝对路径
-    // 下面的 path 就是先根据当前文件的路径，获取到文件的目录的路径，再将目录和文件名拼接成绝对路径
-    const path = resolve(__dirname, src)
-    // 读取文件
-    const data = readFileSync(path, { encoding: 'utf-8' })
-    const corpus = JSON.parse(data)
-    return corpus
-  }
-  // 保存文章
-  function saveCorpus(title, article) {
-    const outputDir = resolve(__dirname, 'output')
-    const time = moment().format('|YYYY-MM-DD|HH:mm:ss')
-    const outputPath = resolve(outputDir, `${title}${time}.txt`)
-
-    const text = `${title}\n\n    ${article.join('\n    ')}`
-    // 如果 output 目录不存在，就创建一个
-    if (!existsSync(outputDir)) {
-      mkdirSync(outputDir)
+  function main() {
+    // 配置我们的命令行参数
+    const optionDefinitions = [
+      { name: 'title', alias: 't', type: String },
+      { name: 'min', type: Number },
+      { name: 'max', type: Number },
+      { name: 'help' },
+    ]
+    // 获取命令行的输入，利用插件，能判断用户输入的参数是否符合规范
+    const options = commandLineArgs(optionDefinitions)
+    console.log(options)
+    // 如果是 --help ，则打印帮助文本
+    if ('help' in options) {
+      console.log(usage)
+      // 退出进程
+      process.exit()
     }
-    // 写入
-    writeFileSync(outputPath, text, { encoding: 'utf-8' })
+
+    // // 获取用户输入，非插件
+    // const options = parseOptions()
+    // 获取语料库
+    const corpus = loadCorpus(`corpus/data.json`)
+    // 获得生成文章标题函数
+    const pickTitle = createRandomPicker(corpus.title)
+    // 生成文章标题
+    const title = options.title || pickTitle()
+    // 生成文章
+    const article = generator(title, { corpus, ...options })
+    // 保存文章
+    saveCorpus(title, article)
+  }
+
+  // 读取参数
+  function parseOptions(options = {}) {
+    const argv = process.argv
+    for (let i = 2; i < argv.length; i++) {
+      const cmd = argv[i - 1]
+      const val = argv[i]
+      if (cmd === '--title') {
+        options.title = val
+      } else if (cmd === '--min') {
+        options.min = Number(val)
+      } else if (cmd === '--max') {
+        options.max = Number(val)
+      }
+    }
+    return options
   }
 })()
